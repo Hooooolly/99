163750fe063bf7f9a1e4a1cfbe7eee7fa9b42845
Unknown <c.xu@wustl.edu>
2018-05-22 04:31:25 -0500

    more progress

--- 99.py
+++ 99.py
@@ -10,6 +10,7 @@ auth_token = '6697edbf300df7cad71886684643b593'
 client = Client(account_sid, auth_token)
 active_numbers = {}
 games = []
+pregame_numbers = {}
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -17,20 +18,40 @@ def incoming_sms():
     send_num = request.values.get('From', None)
     
     message = ''
-    
-    if send_num in active_numbers:
-        message = 'In development'
+    if send_num in pregame_numbers.keys:
+        if pregame_numbers[send_num] == 0:
+            active_numbers[send_num].new_player(body, send_num)
+            message = "Nice to meet you " + body + '. Now please text me the names of the players you would like to add to the game in this format \'Name: Phone Number\''
+            pregame_numbers[send_num] = 1
+        if pregame_numbers[send_num] == 1:
+            if body == 'Done':
+                pregame_numbers.pop(send_num)
+                active_numbers[send_num].start_game()
+            new_name, new_num = body.split(": ")
+            if new_num[0] != '+' | len(new_num) != 12:
+                message = 'Number was not in required format! Needs to be \'+1**********\''
+            else:
+                current_game = active_numbers[send_num]
+                current_game.new_player(new_name, new_num)
+                active_numbers[new_num] = current_game
+                message = 'Great! ' + new_name + ' was added to your game. Text \'Done\' when all players have been added.'
+    elif send_num in active_numbers.keys:
+        if !active_numbers[send_num].started:
+            message = "Oops! Looks like you are in a game that hasn't yet started, hold tight!"
+        else:
+            message = "In Development, Game started!"
     elif body == 'Create Game':
         new_game = Game()
         active_numbers[send_num] = new_game
-        message = 'You have started a game! Please text me the names of the players you would like to add in this format \'Name: Phone Number\''
+        pregame_numbers[send_num] = 0
+        message = 'You have started a game! First, let me know what your name is!'
     else:
         message = 'Welcome to 99! You don\'t seem to be in a game right now, text me \'Create Game\' to get started!'
     
     resp = MessagingResponse()
-
-    #message = send_num + ": " + body
-    resp.message(message)
+    if message != '':
+        #message = send_num + ": " + body
+        resp.message(message)
 
     return str(resp)
 
--- Game.py
+++ Game.py
@@ -18,6 +18,7 @@ class Cards:
 class Game:
     
     def __init__(self):
+        self.started = False
         self.players = []
         self.cards = Cards()
         self.cards.shuffle()
@@ -26,6 +27,7 @@ class Game:
         self.players.append(Player(name, number))
     
     def start_game():
+        self.started = True
         for p in players:
             for _ in range(3):
                 p.cards.append(self.cards.draw_top())
