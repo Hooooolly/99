98d8e0b34945885788e51ebc5a5f88879ebfb6ea
Unknown <c.xu@wustl.edu>
2018-05-22 04:37:06 -0500

    elif

--- 99.py
+++ 99.py
@@ -19,12 +19,12 @@ def incoming_sms():
     
     message = ''
     if send_num in pregame_numbers:
-        print(pregame_numers)
+        print(pregame_numbers)
         if pregame_numbers[send_num] == 0:
             active_numbers[send_num].new_player(body, send_num)
             message = "Nice to meet you " + body + '. Now please text me the names of the players you would like to add to the game in this format \'Name: Phone Number\''
             pregame_numbers[send_num] = 1
-        if pregame_numbers[send_num] == 1:
+        elif pregame_numbers[send_num] == 1:
             if body == 'Done':
                 pregame_numbers.pop(send_num)
                 active_numbers[send_num].start_game()
