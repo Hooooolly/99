17e1fed86261cee6e2c77b8cd7031f27dca511aa
Unknown <c.xu@wustl.edu>
2018-05-22 15:32:04 -0500

    first build

--- 99.py
+++ 99.py
@@ -9,9 +9,9 @@ account_sid = 'ACa77cec72b91ee091ea25a32b97374eaa'
 auth_token = '6697edbf300df7cad71886684643b593'
 client = Client(account_sid, auth_token)
 active_numbers = {}
-games = []
 pregame_numbers = {}
-conversion = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+i2c = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+c2i = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':11, 'Q':12, 'K':13}
 
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
@@ -37,25 +37,56 @@ def incoming_sms():
                 player_one = current_game.players[0]
                 for p in current_game.players:
                     cards = p.cards
-                    cards = [conversion[i] for i in cards]
-                    start_message = "Hello! You've been invited to play 99! You're cards are "
+                    cards = [i2c[i] for i in cards]
+                    start_message = "[TEST_BUILD] Hello! You've been invited to play 99! You're cards are "
                     start_message += str(cards)
                     start_message += ". " + player_one.name + " is up first!"
                     send_message(p.number, start_message)
+                for p in current_game.players:
+                    quick_note = 'If you are trying to play a 10 or A, specify your action by adding a + or - (e.g. A- specifies 1)'
+                    send_message(p.number, quick_note)
             else:
                 new_name, new_num = body.split(": ")
                 if (new_num[0] != '+') | (len(new_num) != 12):
                     message = 'Number was not in required format! Needs to be \'+1**********\''
                 else:
                     current_game = active_numbers[send_num]
-                    current_game.new_player(new_name, new_num)
+                    new_player = Player(new_name, new_num)
+                    current_game.new_player(new_player)
                     active_numbers[new_num] = current_game
                     message = 'Great! ' + new_name + ' was added to your game. Text \'Done\' when all players have been added.'
     elif send_num in active_numbers:
         if not active_numbers[send_num].started:
             message = "Oops! Looks like you are in a game that hasn't yet started, hold tight!"
         else:
-            message = "In Development, Game started!"
+            current_game = active_numbers[send_num]
+            current_player = current_game.next_player()
+            if current_player.number != send_num:
+                message = 'It\'s not your turn yet! Please wait for ' + current_player.name + '.'
+            else:
+                player_cards = current_player.cards
+                player_cards = [i2c[i] for i in player_cards]
+                choice = ''
+                if body == 'A' | body == '10':
+                    choice = body[len(body) - 1:]
+                    body = body[:len(body) - 1]
+                if not body in player_cards:
+                    message = 'That\'s not a valid move! Your cards are ' + str(player_cards) + '.'
+                else:
+                    [stack, reversed, game_over] = current_game.play(c2i[body], choice)
+                    if game_over:
+                        over_message = current_player.name + ' has played a ' + body
+                        over_message += '. This pushed the stack over 99. ' + current_player.name + ' loses!'
+                        for p in current_game.players:
+                            active_numbers.pop(p.number)
+                    else:
+                        for p in current_game.players:
+                            play_message = current_player.name + ' has played a ' + body
+                            play_message += '. The stack value is now ' + stack
+                            if reversed:
+                                play_message += '. The order was reversed'
+                            play_message += '. Your current cards are ' + str(p.cards)
+                            play_message += '. It is now ' + current_player.next_player().name + '\'s turn!'
     elif body == 'Create Game':
         new_game = Game()
         active_numbers[send_num] = new_game
--- Game.py
+++ Game.py
@@ -22,12 +22,49 @@ class Game:
         self.players = []
         self.cards = Cards()
         self.cards.shuffle()
+        self.stack = 0
         
-    def new_player(self, name, number):
-        self.players.append(Player(name, number))
+    def new_player(self, player):
+        self.players.append(player)
+        
+    def next_player(self):
+        return self.players[0]
     
     def start_game(self):
         self.started = True
         for p in self.players:
             for _ in range(3):
                 p.cards.append(self.cards.draw_top())
+    
+    def play(self, card, choice):
+        reverse = False
+        if card == 1:
+            if choice == '+':
+                self.stack += 11
+            else:
+                self.stack += 1
+        if card == 4:
+            reverse = True
+            self.stack = self.stack
+        if card == 9:
+            self.stack = self.stack
+        if card == 10:
+            if choice == '+':
+                self.stack += 10
+            else
+                self.stack -= 10
+        if card == 11 | card == 12:
+            self.stack += 10
+        if card == 13:
+            self.stack = 99
+        if stack > 99:
+            return [stack, reverse, True]
+        else:
+            self.players[0].remove(card)
+            self.players[0].append(self.deck.draw_top())
+            current_player = self.players.pop(0)
+            if reverse:
+                self.players.reverse()
+            self.players.append(current_player)
+            return [stack, reverse, False]
+        
